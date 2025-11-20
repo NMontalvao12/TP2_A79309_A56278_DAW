f03d8bf6b8f65e2ed750eed618463395071c719a
Afonso Figueiredo <120734751+NMontalvao12@users.noreply.github.com>
2025-11-20 19:11:05 +0000

    project start

---
+++ index.js
@@ -0,0 +1,10 @@
+// index.js
+const express = require('express');
+const app = express();
+const port = process.env.PORT;
+
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
